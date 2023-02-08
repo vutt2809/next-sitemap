271e1fb551c90fdec1c1e42ca369040c0d5f932e
vutt2809 <tranthanhvu2809@gmail.com>
2023-02-08 17:16:55 +0700

    init source

---
+++ utils/api.js
@@ -0,0 +1,42 @@
+export const api = [
+    {
+        type_id: '2',
+        content_id: '17251',
+    },
+    {
+        type_id: '2',
+        content_id: '17552',
+    },
+    {
+        type_id: '2',
+        content_id: '19271',
+    },
+    {
+        type_id: '2',
+        content_id: '15643',
+    },
+    {
+        type_id: '2',
+        content_id: '16725',
+    },
+    {
+        type_id: '20',
+        content_id: '19261',
+    },
+    {
+        type_id: '20',
+        content_id: '14233',
+    },
+    {
+        type_id: '20',
+        content_id: '17865',
+    },
+    {
+        type_id: '20',
+        content_id: '18943',
+    },
+    {
+        type_id: '20',
+        content_id: '19054',
+    },
+]
